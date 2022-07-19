5fb7531a6628c5550624aaf339afde56ac955352
Tomás Castillo <tomrndom@users.noreply.github.com>
2022-07-19 11:29:46 -0300

    Change font family on the site (#28)
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/RegistrationLiteComponent.js
+++ src/components/RegistrationLiteComponent.js
@@ -145,7 +145,7 @@ const RegistrationLiteComponent = ({
         allowsOtpAuth: allowsOtpAuth,
         stripeOptions: {
             fonts: [{ cssSrc: withPrefix('/fonts/fonts.css') }],
-            style: { base: { fontFamily: `'Nunito Sans', sans-serif`, fontWeight: 300 } }
+            style: { base: { fontFamily: `'AvenirRegular', sans-serif`, fontWeight: 300 } }
         }
     };
 
