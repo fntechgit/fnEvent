19f6f7934ee764b0b53271335014fa84b0c81588
smarcet <smarcet@gmail.com>
2022-10-12 01:40:52 -0300

    Removed fingerprints
    Signed-off-by: smarcet <smarcet@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: 'Virtual event',
   },
   plugins: [
+    `gatsby-plugin-remove-fingerprints`,
     'gatsby-plugin-react-helmet',
     {
       resolve: "gatsby-plugin-no-sourcemaps"
