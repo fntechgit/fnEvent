c5602bef0b9d852f874ebce73866eaf2b74a7d3d
smarcet <smarcet@gmail.com>
2023-02-27 17:03:44 -0300

    Added new login callback url *  auth/login
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/pages/auth/[...].js
+++ src/pages/auth/[...].js
@@ -3,10 +3,12 @@ import { Router } from "@reach/router"
 import { connect } from 'react-redux'
 
 import { onUserAuth, doLogout, getUserInfo } from "openstack-uicore-foundation/lib/security/actions";
+import { doLoginBasicLogin } from 'openstack-uicore-foundation/lib/security/methods';
 import TokenExpirePage from "../../templates/token-expire-page"
 
 import AuthorizationCallbackRoute from "../../routes/authorization-callback-route"
 import LogOutCallbackRoute from "../../routes/logout-callback-route"
+import LogInCallbackRoute from "../../routes/login-callback-route";
 
 const Auth = class extends React.Component {
 
@@ -18,6 +20,7 @@ const Auth = class extends React.Component {
       <Router basepath="/auth">
         <AuthorizationCallbackRoute onUserAuth={onUserAuth} path='/callback' getUserInfo={getUserInfo} />
         <LogOutCallbackRoute doLogout={doLogout} path='/logout' />
+        <LogInCallbackRoute doLogin={doLoginBasicLogin} path='/login'/>
         <TokenExpirePage path="/expired" location={location} />
       </Router>
     )
---
+++ src/routes/login-callback-route.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2023 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+import URI from "urijs"
+import React from 'react'
+import {withRouter} from 'react-router-dom'
+import {connect} from "react-redux";
+
+/**
+ * class LogInCallbackRoute
+ */
+class LogInCallbackRoute extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: null
+        };
+    }
+
+    componentWillMount() {
+        const {doLogin, location} = this.props;
+        const query = URI.parseQuery(location.search);
+        let loginHint = null;
+        let otpLoginHint = null;
+        let backUrl = '/';
+
+        if (query["login_hint"]) {
+            loginHint = encodeURI(query["login_hint"]);
+        }
+
+        if (query["backUrl"]) {
+            backUrl = encodeURI(query["backUrl"]);
+        }
+
+        if (query["otp_login_hint"]) {
+            otpLoginHint = encodeURI(query["otp_login_hint"]);
+        }
+
+        doLogin(backUrl, loginHint, otpLoginHint);
+    }
+
+
+    render() {
+        if (this.state.error != null) {
+            return (<p>{this.state.error}</p>)
+        }
+        return null;
+    }
+}
+
+export default connect(null)(LogInCallbackRoute);
