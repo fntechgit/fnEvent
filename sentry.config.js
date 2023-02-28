e6ccdc7b666bde1bdc56d96bb4b477e5a6e2d84f
smarcet <smarcet@gmail.com>
2023-02-28 17:55:17 -0300

    Fixes on relase
    Signed-off-by: smarcet <smarcet@gmail.com>

--- sentry.config.js
+++ sentry.config.js
@@ -1,12 +1,12 @@
 import * as Sentry from "@sentry/gatsby";
 
 Sentry.init({
-  dsn: process.env.GATSBY_SENTRY_DSN,
-  tracesSampleRate: process.env.GATSBY_SENTRY_TRACE_SAMPLE_RATE,
+  dsn : process.env.GATSBY_SENTRY_DSN,
+  tracesSampleRate : process.env.GATSBY_SENTRY_TRACE_SAMPLE_RATE,
   beforeSend(event) {
     // Modify the event here
     console.log('before send...', event)    
     return event;
   },
-  // ...
+  release : process.env.BUILD_ID,
 });
