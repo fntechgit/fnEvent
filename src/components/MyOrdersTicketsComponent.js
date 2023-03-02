1279ac9de3728eac9eb4f9bb0c64f81edcef10f1
Tomás Castillo <tomrndom@users.noreply.github.com>
2023-03-02 10:22:08 -0300

    Add sentry lib, new error boundaries for widgets (#124)
    * Add sentry lib, new error boundaries for widgets
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Clean sentry beforeSend fucntion
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Add new env variables, env template, more info on error message
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Add source maps
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Upload source maps for production only
    
    * Update testing sentry implementation
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Tweaks
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * more config tweaks
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * FIX
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixes on relase
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixes
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixes on source map
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixes on maps
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixes on sentry config
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixes
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fix on sentry config
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Removed no source map plugin
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixed frames
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Added error
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixed sentry config
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fix on config
    removed error
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    * Fixes on config
    
    Signed-off-by: smarcet <smarcet@gmail.com>
    
    ---------
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    Signed-off-by: smarcet <smarcet@gmail.com>
    Co-authored-by: smarcet <smarcet@gmail.com>

--- src/components/MyOrdersTicketsComponent.js
+++ src/components/MyOrdersTicketsComponent.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import * as Sentry from "@sentry/react";
 import { useDispatch, useSelector } from 'react-redux';
 import { getAccessToken } from 'openstack-uicore-foundation/lib/security/methods';
 import { getEnvVariable, IDP_BASE_URL, SUMMIT_API_BASE_URL, OAUTH2_CLIENT_ID, SUPPORT_EMAIL } from '../utils/envVariables';
 import { MyOrdersTicketsWidget } from './summit-my-orders-tickets';
 import { getUserProfile } from '../actions/user-actions';
+
+import { SentryFallbackFunction } from "./SentryErrorComponent";
 
 export const MyOrdersTicketsComponent = () => {
     const dispatch = useDispatch();
@@ -25,6 +28,8 @@ export const MyOrdersTicketsComponent = () => {
     };
 
     return (
-        <MyOrdersTicketsWidget {...widgetProps} />
+        <Sentry.ErrorBoundary fallback={SentryFallbackFunction({componentName: 'My Orders & Tickets'})}>
+            <MyOrdersTicketsWidget {...widgetProps} />
+        </Sentry.ErrorBoundary>
     );
 };
