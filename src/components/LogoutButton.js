d480b6ff262994ac0991a223da734c4913e73898
smarcet <smarcet@gmail.com>
2023-03-03 13:03:18 -0300

    Fix on logout action
    trigger a custom event to hook up a2a cleanning process
    
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { navigate } from "gatsby"
+import {triggerOnInitLogout} from "../utils/customEvents";
 
 export default class
   LogoutButton extends React.Component {
 
   onClickLogout() {
+    triggerOnInitLogout();
     navigate('/auth/logout', {
       state: {
         backUrl: window.location.pathname
@@ -19,17 +21,6 @@ export default class
       return (
 
         <div className={styles.buttons}>
-          {/*
-          <a className={`${styles.userIcon}`}>
-            <i className="fa fa-exclamation-circle icon is-medium" style={{fontSize: '1.5rem'}} />
-          </a>
-          <a className={`${styles.userIcon}`}>
-            <i className="fa fa-cog icon is-medium" style={{fontSize: '1.5rem'}} />
-          </a>
-          <a className={`${styles.userIcon}`}>
-            <i className="fa fa-bell icon is-medium" style={{fontSize: '1.5rem'}} />
-          </a>
-          */}
           <button className={`link ${styles.userIcon}`} title="Logout" onClick={() => this.onClickLogout()}>
             <i className="fa fa-sign-out icon is-medium" style={{fontSize: '1.5rem'}} />
           </button>
