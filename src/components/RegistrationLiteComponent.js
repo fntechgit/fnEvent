7528e6e96b633f4cc53269a665fd25993a995922
sebastian marcet <smarcet@gmail.com>
2023-03-28 09:58:59 -0300

    Updated reg lite v4.0.7 (#141)
    * fixed last step ( purchase step) logic
    * fixed user reducer bug
    
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components/RegistrationLiteComponent.js
+++ src/components/RegistrationLiteComponent.js
@@ -15,6 +15,7 @@ import 'summit-registration-lite/dist/index.css';
 import styles from '../styles/marketing-hero.module.scss'
 import Swal from "sweetalert2";
 import {checkRequireExtraQuestionsByAttendee} from "../actions/user-actions";
+import {userHasAccessLevel, VirtualAccessLevel} from "../utils/authorizedGroups";
 
 const RegistrationLiteComponent = ({
                                        registrationProfile,
@@ -98,6 +99,7 @@ const RegistrationLiteComponent = ({
         loading: loadingProfile || loadingIDP,
         // only show info if its not a recent purchase
         ticketOwned: userProfile?.summit_tickets?.length > 0,
+        hasVirtualAccessLevel: userHasAccessLevel(userProfile?.summit_tickets, VirtualAccessLevel),
         ownedTickets: attendee?.ticket_types || [],
         authUser: (provider) => onClickLogin(provider),
         getPasswordlessCode: getPasswordlessCode,
